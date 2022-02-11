6b952e213eb218ccad3ae870a4a2334019c45612
okcomper <okcomper@naver.com>
2022-02-11 09:36:44 +0900

    add : PersonDTO

---
+++ imagine/PersonoDTO.java
@@ -0,0 +1,42 @@
+package book.model.dto;
+
+public class PersonoDTO {
+	private String pname;
+	private String pid;
+	private String password;
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("1. 대여자 id : ");
+		builder.append(pid);
+		builder.append(" 2. 이름 : ");
+		builder.append(pname);
+		builder.append(" 3. 비밀번호 : ");
+		builder.append(password);
+		return builder.toString();
+	}
+	public PersonoDTO(String pname, String pid, String password) {
+		super();
+		this.pname = pname;
+		this.pid = pid;
+		this.password = password;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getPid() {
+		return pid;
+	}
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}	
