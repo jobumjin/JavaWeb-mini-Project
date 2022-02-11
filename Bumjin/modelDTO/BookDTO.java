3b0bbe31e389782a4cb24f2eb5449df6d0db3029
okcomper <97003068+okcomper@users.noreply.github.com>
2022-02-11 10:22:57 +0900

    Update BookDTO.java

--- Bumjin/modelDTO/BookDTO.java
+++ Bumjin/modelDTO/BookDTO.java
@@ -1,49 +1,55 @@
 package book.model.dto;
 
-public class BookDTO {
-	private String bname;
-	private String Bnumber;
-	private String publisher;
+public class PersonDTO {
+	private String pname;
+	private String pid;
+	private String password;	
+	private String interest;
+	public PersonDTO(String pname, String pid, String password, String interest) {
+		super();
+		this.pname = pname;
+		this.pid = pid;
+		this.password = password;
+		this.interest = interest;
+	}
 	
-	public BookDTO() {}	
-	
-	public BookDTO(String bname, String bnumber, String barely) {
-		this.bname = bname;
-		this.Bnumber = bnumber;
-		this.publisher = publisher;
+	public String getPname() {
+		return pname;
 	}
-
-	public String getbname() {
-		return bname;
+	public void setPname(String pname) {
+		this.pname = pname;
 	}
-	public void setbname(String bname) {
-		bname = bname;
+	public String getPid() {
+		return pid;
 	}
-	public String getBnumber() {
-		return Bnumber;
+	public void setPid(String pid) {
+		this.pid = pid;
 	}
-	public void setBnumber(String bnumber) {
-		Bnumber = bnumber;
+	public String getPassword() {
+		return password;
 	}
-	public String getPublisher() {
-		return publisher;
+	public void setPassword(String password) {
+		this.password = password;
 	}
-	public void setPublisher(String barely) {
-		this.publisher = barely;
+	public String getInterest() {
+		return interest;
 	}
-
+	public void setInterest(String interest) {
+		this.interest = interest;
+	}
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("1. 책 이름");
-		builder.append(bname);
-		builder.append("2. 책 고유번호");
-		builder.append(Bnumber);
-		builder.append("3. 출판사");
-		builder.append(publisher);
+		builder.append("1. 대여자 id : ");
+		builder.append(pid);
+		builder.append(" 2. 이름 : ");
+		builder.append(pname);
+		builder.append(" 3. 비밀번호 : ");
+		builder.append(password);
+		builder.append("4. 관심분야 :");
+		builder.append(interest);
 		return builder.toString();
 	}
 	
-	
-	
+
 }
